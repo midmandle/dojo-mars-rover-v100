f70a1a4b1b9f1705436699fe3bc8b03e186f3971
alasdairsmith <alasdairsmith@>
2024-02-22 20:15:02 +0000

    Init

--- src/lib/mars-rover.spec.ts
+++ src/lib/mars-rover.spec.ts
@@ -1,7 +1,26 @@
-import { marsRover } from './mars-rover';
+import { MarsRover } from './mars-rover';
 
 describe('marsRover', () => {
-  it('should work', () => {
-    expect(marsRover()).toEqual('mars-rover');
+  it('should start at 0:0:N', () => {
+
+    const marsRover = new MarsRover();
+
+    expect(marsRover.execute("")).toEqual('0:0:N');
+  });
+
+  it.each([
+    ['R','0:0:E'],
+    ['RR','0:0:S'],
+    ['RRR','0:0:W'],
+    ['RRRRR','0:0:E'],
+    ['L','0:0:W'],
+    ['LL','0:0:S'],
+    ['LLL','0:0:E'],
+    ['LLLLL','0:0:W'],
+  ])('should rotate %s to face %s', (input: string, output: string) => {
+
+    const marsRover = new MarsRover();
+
+    expect(marsRover.execute(input)).toEqual(output);
   });
 });
--- src/lib/mars-rover.ts
+++ src/lib/mars-rover.ts
@@ -1,3 +1,34 @@
-export function marsRover(): string {
-  return 'mars-rover';
+const TURN_RIGHT = 'R';
+const TURN_LEFT = 'L'
+
+export class MarsRover {
+  private currentDirection = 'N';
+  execute(commands: string) {
+    const xCoordinate = '0';
+    const yCoordinate = '0';
+    const directionArr = ['N', 'E', 'S', 'W'];
+
+    if (commands.includes('R')) {
+     this.turnRight(directionArr, commands);
+    }
+    if (commands.includes('L')) {
+      this.turnLeft(directionArr, commands);
+    }
+
+    return this.generateOutputString(xCoordinate, yCoordinate);
+  }
+
+  private turnRight(directionArr: string[], commands: string) {
+    this.currentDirection = directionArr[commands.length % 4];
+  }
+
+  private turnLeft(directionArr: string[], commands: string) {
+      this.currentDirection = directionArr[(4 -commands.length % 4)];
+
+
+  }
+
+  private generateOutputString(xCoordinate: string, yCoordinate: string) {
+    return `${xCoordinate}:${yCoordinate}:${this.currentDirection}`;
+  }
 }
